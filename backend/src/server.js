846bb8696771369bb595facdf2ce0d04bed9fa59
diogoosz <diogomarciano40@gmail.com>
2025-12-07 22:16:17 -0300

    fix railway backend subfolder config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 require('dotenv').config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.MYSQLPORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
