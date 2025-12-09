c0a394d4e0bddd9e48a0c19e81a47def004bbc25
diogoosz <diogomarciano40@gmail.com>
2025-12-08 22:11:51 -0300

    atualizando o javascript do front

---
+++ frontend/js/script.js
@@ -0,0 +1,60 @@
+const tBody = document.querySelector('tbody');
+
+async function fetchTasks() {
+  const resp = await fetch(
+    "https://create-tasks-api-production.up.railway.app/tasks",
+    { method: "GET", mode: "cors", headers: { "Content-Type": "application/json" } }
+  );
+  return await resp.json();
+}
+
+const createElement = (tag, innerText = '', innerHTML = '') => {
+    const element = document.createElement(tag);
+    element.innerText = innerText;
+    if (innerHTML) {
+        element.innerHTML = innerHTML;
+    }
+    return element;
+}
+
+const createSelect = (value) => {
+    const options = `
+        <option value="pendente">Pendente</option>
+        <option value="em andamento">Em andamento</option>
+        <option value="concluída">Concluída</option>
+    `;
+
+    const select = createElement('select', '', options);
+
+    select.value = value || 'pendente';
+
+    return select;
+}
+
+const createRow = (task) => {
+    const {id, title, status, created_at} = task;
+    const tr = createElement('tr');
+    const tdTitle    = createElement('td', title);
+    const tdStatus   = createElement('td');
+    const tdCreated  = createElement('td', new Date(created_at).toLocaleDateString('pt-BR'));
+    const tdActions  = createElement('td');
+
+    const select = createSelect(status);
+
+    const editButton = createElement('button', '', '<span class="material-symbols-outlined">edit</span>');
+    const deleteButton = createElement('button', '', '<span class="material-symbols-outlined">delete</span>');
+
+    editButton.classList.add('btn-action');
+    deleteButton.classList.add('btn-action');
+
+    tdStatus.appendChild(select);   
+
+    tdActions.appendChild(editButton);
+    tdActions.appendChild(deleteButton);
+
+    tr.appendChild(tdTitle);
+    tr.appendChild(tdStatus);
+    tr.appendChild(tdCreated);
+    tr.appendChild(tdActions);
+    
+}
