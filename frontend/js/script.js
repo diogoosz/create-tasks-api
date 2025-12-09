e4ffcd6aacb7717f6c837429953e1d046d5c16ac
Diogo <diogomarciano40@gmail.com>
2025-12-09 10:57:19 -0300

    nova função inserir task

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,60 +1,119 @@
-const tBody = document.querySelector('tbody');
+const tBody = document.querySelector("tbody");
+const addForm = document.querySelector(".add-form");
+const inputTask = document.querySelector(".input-task");
 
 async function fetchTasks() {
-  const resp = await fetch(
-    "https://create-tasks-api-production.up.railway.app/tasks",
-    { method: "GET", mode: "cors", headers: { "Content-Type": "application/json" } }
-  );
-  return await resp.json();
+  try {
+    const resp = await fetch(
+      "https://create-tasks-api-production.up.railway.app/tasks",
+      {
+        method: "GET",
+        mode: "cors",
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    return await resp.json();
+  } catch (error) {
+    console.log("Não foi possivel buscar as tasks: ", error);
+    throw error;
+  }
 }
 
-const createElement = (tag, innerText = '', innerHTML = '') => {
-    const element = document.createElement(tag);
-    if (innerText) {
-        element.innerText = innerText;
-    } else {
-        element.innerHTML = innerHTML;
-    }
-    return element;
+async function addTask(event) {
+  event.preventDefault();
+  const task = { title: inputTask.value };
+  try {
+    const resp = await fetch(
+      "https://create-tasks-api-production.up.railway.app/tasks",
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(task),
+      }
+    );
+    return await resp.json();
+  } catch (error) {
+    console.log("Erro ao criar task: ", error);
+    throw error;
+  }
 }
+
+const createElement = (tag, innerText = "", innerHTML = "") => {
+  const element = document.createElement(tag);
+  if (innerText) {
+    element.innerText = innerText;
+  } else {
+    element.innerHTML = innerHTML;
+  }
+  return element;
+};
 
 const createSelect = (value) => {
-    const options = `
+  const options = `
         <option value="pendente">Pendente</option>
         <option value="em andamento">Em Andamento</option>
         <option value="concluída">Concluída</option>
     `;
-    
-    const select = createElement('select', '', options);
 
-    select.value = value;
+  const select = createElement("select", "", options);
 
-    return select;
-}
+  select.value = value;
+
+  return select;
+};
 
 const createRow = (task) => {
-    const {id, title, status, created_at} = task;
-    const tr = createElement('tr');
-    const tdTitle    = createElement('td', title);
-    const tdStatus   = createElement('td');
-    const tdCreated  = createElement('td', new Date(created_at).toLocaleDateString('pt-BR'));
-    const tdActions  = createElement('td');
+  const { id, title, status, created_at } = task;
+  const tr = createElement("tr");
+  const tdTitle = createElement("td", title);
+  const tdStatus = createElement("td");
+  const tdCreated = createElement(
+    "td",
+    new Date(created_at).toLocaleDateString("pt-BR")
+  );
+  const tdActions = createElement("td");
 
-    const select = createSelect(status);
+  const select = createSelect(status);
 
-    const editButton = createElement('button', '', '<span class="material-symbols-outlined">edit</span>');
-    const deleteButton = createElement('button', '', '<span class="material-symbols-outlined">delete</span>');
+  const editButton = createElement(
+    "button",
+    "",
+    '<span class="material-symbols-outlined">edit</span>'
+  );
+  const deleteButton = createElement(
+    "button",
+    "",
+    '<span class="material-symbols-outlined">delete</span>'
+  );
 
-    editButton.classList.add('btn-action');
-    deleteButton.classList.add('btn-action');
+  editButton.classList.add("btn-action");
+  deleteButton.classList.add("btn-action");
 
-    tdStatus.appendChild(select);   
+  tdStatus.appendChild(select);
 
-    tdActions.appendChild(editButton);
-    tdActions.appendChild(deleteButton);
+  tdActions.appendChild(editButton);
+  tdActions.appendChild(deleteButton);
 
-    tr.appendChild(tdTitle);
-    tr.appendChild(tdStatus);
-    tr.appendChild(tdCreated);
-    tr.appendChild(tdActions);
-}
+  tr.appendChild(tdTitle);
+  tr.appendChild(tdStatus);
+  tr.appendChild(tdCreated);
+  tr.appendChild(tdActions);
+
+  return tr;
+};
+
+const loadTasks = async () => {
+  const tasks = await fetchTasks();
+
+  tasks.forEach((task) => {
+    const tr = createRow(task);
+    tBody.appendChild(tr);
+  });
+};
+
+addForm.addEventListener("submit", addTask);
+loadTasks();
