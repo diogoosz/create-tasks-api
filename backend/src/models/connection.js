fc6c6e610e78c367865159b4723407aeacf1049e
diogoosz <diogomarciano40@gmail.com>
2025-12-27 11:51:29 -0300

    atuzliando env

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDB,
+    database: process.env.MYSQL_DATABASE,
     port: Number(process.env.MYSQLPORT),
 });
 
